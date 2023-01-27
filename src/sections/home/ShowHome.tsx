cc0306c940e468eac74f991109215b43226ee878
Brad Bartlett <bbartlett222@gmail.com>
2023-01-27 17:08:57 -0500

    temp

--- src/sections/home/ShowHome.tsx
+++ src/sections/home/ShowHome.tsx
@@ -28,7 +28,7 @@ function ShowHome() {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: `write an original article about ${article.title} ${article.description}`,
-      temperature: 0.8,
+      temperature: 0.9,
       max_tokens: 800,
     });
     setGeneratedArticle(response.data.choices[0].text);
