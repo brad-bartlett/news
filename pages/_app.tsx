98533118c6405632e75c387bc40c640d4e95cb9f
Brad Bartlett <bbartlett222@gmail.com>
2023-02-02 18:15:16 -0500

    fix head and add navbar to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
+import NavBar from "@/src/components/NavBar";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import "../globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>NewsCorp</title>
+      </Head>
+      <NavBar />
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,12 @@
 import React from "react";
-import Head from "next/head";
 import ShowHome from "@/src/sections/home";
-import NavBar from "@/src/components/NavBar";
 import PageWrapper from "@/src/commons/PageWrapper";
 
 function Home() {
   return (
-    <>
-      <Head>
-        <title>NewsCorp</title>
-      </Head>
-      <NavBar />
-      <PageWrapper>
-        <ShowHome />
-      </PageWrapper>
-    </>
+    <PageWrapper>
+      <ShowHome />
+    </PageWrapper>
   );
 }
 
