9e7257422ef2cfe34f8a9299d6c912671a96f6ee
Brad Bartlett <bbartlett222@gmail.com>
2023-02-04 13:43:04 -0500

    grid on card

--- src/commons/ArticleCard/ArticleCard.tsx
+++ src/commons/ArticleCard/ArticleCard.tsx
@@ -9,16 +9,18 @@ interface ArticleCardProps {
 function ArticleCard({ title, urlToImage }: ArticleCardProps) {
   return (
     <div className="grid grid-flow-row auto-rows-min">
-      {urlToImage && (
-        <Image
-          src={urlToImage}
-          width={200}
-          height={200}
-          alt={title}
-          className="rounded-lg w-full"
-        />
-      )}
-      <h2 className="text-lg font-bold">{title}</h2>
+      <div>
+        {urlToImage && (
+          <Image
+            src={urlToImage}
+            width={200}
+            height={200}
+            alt={title}
+            className="rounded-lg w-full h-44"
+          />
+        )}
+      </div>
+      <h2 className="text-lg font-bold text-ellipsis">{title}</h2>
     </div>
   );
 }
