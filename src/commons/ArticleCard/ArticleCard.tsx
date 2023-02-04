29af2f8e4057291c6a840696ef6704aea4d1dac9
Brad Bartlett <bbartlett222@gmail.com>
2023-02-04 13:48:56 -0500

    image placeholder

--- src/commons/ArticleCard/ArticleCard.tsx
+++ src/commons/ArticleCard/ArticleCard.tsx
@@ -10,11 +10,19 @@ function ArticleCard({ title, urlToImage }: ArticleCardProps) {
   return (
     <div className="grid grid-flow-row auto-rows-min">
       <div>
-        {urlToImage && (
+        {urlToImage ? (
           <Image
             src={urlToImage}
-            width={200}
-            height={200}
+            width={176}
+            height={176}
+            alt={title}
+            className="rounded-lg w-full h-44"
+          />
+        ) : (
+          <Image
+            src="https://images.unsplash.com/photo-1626808936965-121820669d25?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1758&q=80"
+            width={176}
+            height={176}
             alt={title}
             className="rounded-lg w-full h-44"
           />
