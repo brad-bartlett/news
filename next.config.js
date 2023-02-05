8b9da5d3c26bbdc2424bcfb583b4c6521ecbc990
Brad Bartlett <bbartlett222@gmail.com>
2023-02-04 20:55:30 -0500

    next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
       {
         protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
         hostname: "**",
       },
     ],
