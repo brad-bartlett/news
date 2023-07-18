765937480a8e38cc94a95d4a642641d319d75390
Brad Bartlett <bradbartlett@Brads-MBP.attlocal.net>
2023-07-18 16:53:12 -0400

    chakra installation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,12 @@ import NavBar from "@/src/components/NavBar";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { Provider } from "jotai";
+import { ChakraProvider } from '@chakra-ui/react'
 import "../globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ChakraProvider>
       <Head>
         <title>NewsCorp</title>
       </Head>
@@ -14,6 +15,6 @@ export default function App({ Component, pageProps }: AppProps) {
         <NavBar />
         <Component {...pageProps} />
       </Provider>
-    </>
+    </ChakraProvider>
   );
 }
